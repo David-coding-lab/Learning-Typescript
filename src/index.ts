ec806a0a3a5ef345c93b901ecbb4c91f2b09d88d
Emeka David <emekad745@gmal.com>
2026-02-09 16:46:49 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let age: number = 10;
+// this (☝️☝️ age: number ) is called annotation. we annotated age to be a number
+
+// Debugging lesson
+if (age < 50 )
+    age += 10;
+
+console.log(age)
+
+
