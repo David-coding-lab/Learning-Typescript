b5a94c4d8f3c607449c45b0b42f171ebbc855da4
Emeka David <emekad745@gmal.com>
2026-02-09 16:47:03 +0100

    Fix .gitignore and remove dist from tracking

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let age = 10;
-// this (☝️☝️ age: number ) is called annotation. we annotated age to be a number
-// Debugging lesson
-if (age < 50)
-    age += 10;
-console.log(age);
-//# sourceMappingURL=index.js.map
