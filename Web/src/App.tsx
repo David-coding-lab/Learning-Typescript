3ac68f71704375dd544070e94d87d66458e96d1f
Emeka David <emekad745@gmal.com>
2026-02-22 17:35:18 +0100

    added automatic image gen based on weather

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -3,12 +3,16 @@ import './App.css'
 import FetchImg from './services/FetchImg'
 
 function App() {
-  
   return (
     <div
       className='container'
       style={{
-        backgroundImage: `url(${FetchImg('rainy')})`
+        backgroundImage: `url(${FetchImg('windy')})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100vw'
       }}
     >
       
--- Web/src/services/FetchImg.tsx
+++ Web/src/services/FetchImg.tsx
@@ -2,10 +2,16 @@ import weatherImages from "../components/weatherImgs";
 
 export default function FetchImg(weather: string) {
     const weatherType = weather; 
+    const imgs: string[] = []
    
-    const weatherImgUrl = weatherImages.find(weatherObj => weatherType === weatherObj.weather)
-    if (!weatherImgUrl){
-        throw new Error("Something Went Wrong");
+    // const weatherImgUrl = weatherImages.find(weatherObj => weatherType === weatherObj.weather)
+
+    for (let index = 0; index < weatherImages.length; index++) {
+        const weatherImgUrl = weatherImages[index];
+        if(weatherImgUrl.weather === weatherType){
+            imgs.push(weatherImgUrl.img)
+        }
     }
-    return weatherImgUrl
+    const randomIndex = Math.floor(Math.random() * (imgs.length));
+    return imgs[randomIndex]
 }
